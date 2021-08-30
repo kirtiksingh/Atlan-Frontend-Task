340238061e1158db91d98bd65e450c7c9aa0306d
Kirtik Singh <kirtiksingh8@gmail.com>
2021-08-30 14:26:50 +0530

    CSS, PWA Fixes

--- src/components/editor-components/Header.js
+++ src/components/editor-components/Header.js
@@ -1,13 +1,16 @@
-import logo from '../../assets/logo.svg'
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
 
 const Header = () => {
   return (
     <div className='header'>
       <div className='logo'>
-        <img src={logo} width="100px" height="100%" alt="atlan logo" />
+        <Link to='/'>
+          <img src={logo} width='100px' height='100%' alt='atlan logo' />
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
